fab4cda734ef941c35bbcc3988d03a890f8d8510
Kumar Kush <krkush23@utlook.com>
2021-03-27 00:07:22 +0530

    Prepare for deploy

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-//post_request -> submitting data,like registering a user
+const User = require('../models/User');
 
-// ===== SIGNATURES ==============================
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
+router.post(
+  '/',
+  [
+    //Validation Checks
+    check('name', 'Please add name').not().isEmpty(), //field, msg if error, checks
+    check('email', 'Please include a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //check for validation errors
+      return res.status(400).json({ errors: errors.array() }); //returns error in json format as array of errors
+    }
 
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+    const { name, email, password } = req.body;
+
+    try {
+      // let user = await User.findOne({email: email}) // since ES6 we can use just email as both have same names
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      // hashed passwrod
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000, //usually 36000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require('express'); //bringing in express
-// above syntax is called COMMON_JS,
-// we can use 'import' only after implementing babel or typescript
-// react uses ES6 modules that has 'import'
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the ContactKeeper API...' })
-);
-// app.get('/', (req, res) => res.send('Hello World'));
-// one way to send data, can put  HTML too
-// sendFile() -> to send file
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// app.get('/', (req, res) =>
+//   res.json({ msg: 'Welcome to the ContactKeeper API...' })
+// );
 
 // ==== DEFINE ROUTES =================================
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
